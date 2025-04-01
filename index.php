adc5cd02d5cbd3a41bebf9581d6e06d99775c219
Fauzan <fauzan224321@gmail.com>
2025-04-01 09:59:17 +0700

    Set font to Rubik

--- index.php
+++ index.php
@@ -1,32 +1,40 @@
 <?php 
-    include './config/database.php';
     include './includes/header.php';
-
-    $stmt = $conn->query("SELECT * FROM user");
-
-    $data = $stmt->fetch_assoc();
-
-    foreach($data as $row){
-        echo "ID : " . $row["id"] . " - Username : " . $row['user'] . "<br>";
-    }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="id">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RyuzenISP - Internet Cepat dan Stabil</title>
+
+    <meta name="description" content="Ryuzen ISP menyediakan layanan internet cepat, stabil, dan terjangkau.">
+    <meta name="keywords" content="ISP, Ryuzen, Internet Cepat, Provider Internet, WiFi Murah, Internet Stabil">
+    <meta name="robots" content="index, follow">
+    
+    <meta property="og:title" content="Ryuzen ISP - Layanan Internet Cepat dan Stabil">
+    <meta property="og:description" content="Solusi terbaik untuk internet rumah dan bisnis.">
+    <meta property="og:image" content="http://localhost/logo.png">
+    <meta property="og:url" content="http://localhost">
+    
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Ryuzen ISP - Internet Cepat dan Stabil">
+    <meta name="twitter:description" content="Layanan internet terbaik untuk rumah dan bisnis.">
+    <meta name="twitter:image" content="http://localhost/logo.png">
+    <!-- CSS -->
+     <link rel="stylesheet" href="./assets/css/index.css">
 </head>
 <body>
-
+       
 
 
     <!-- Footer -->
      <?php include './includes/footer.php';?>
 
      <!-- ScrollReveal -->
-    <script src="https://unpkg.com/srollreveal"></script>
+     <script src="https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js"></script>
     <!-- Javascript -->
      <script src="./assets/js/main.js"></script>
 </body>
